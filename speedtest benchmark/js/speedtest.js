b5f7ca2c57934c953434c16aba90194fbd54015d
ZawHtetNaung <50049906+ZawHtetNaung@users.noreply.github.com>
2021-10-18 15:57:04 +0630

    Add files via upload

---
+++ speedtest benchmark/js/speedtest.js
@@ -0,0 +1,20 @@
+class Speedtest {
+    download_speed() {}
+    upload_speed() {}
+    ping() {}
+}
+
+class Benchmarkinterface {
+    hourly() {}
+    download_average(date) {}
+    upload_average(date) {}
+    ping_average(date) {}
+}
+
+class Benchmark {
+    download(date) {}
+    upload(date) {}
+    ping(date) {}
+}
+
+
